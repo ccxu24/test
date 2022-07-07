d9d1b19a4cb266f7a65e5212fab93c0ae97ec17f
ccxu24 <ccxu@cps.edu>
2022-07-07 14:33:24 -0500

    added day 3

---
+++ day-3/sheep.js
@@ -0,0 +1,13 @@
+let sheeps = [
+"sheep number 1",
+"sheep number 2",
+"sheep number 3",
+"sheep number 4",
+"sheep number 5"
+];
+
+for (let i=0; i <=4; i = i + 1) {
+    console.log(sheeps [i]);
+    
+}
+
